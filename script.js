c12167a43a9eae2c7fd4b5c41bcd44ce7b9ea59a
Ricky Gobby <ricky@Rickys-MacBook-Pro.local>
2021-04-14 14:41:28 +0800

    added delayed fade in for the loaded events to make website more aesthetic

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ $(document).ready(function () {
         e.preventDefault();
         let input = document.getElementById("input").value;
         city = input;
-        console.log(city);
-        console.log(input);
         requestUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=MHiKGa2w6tWsHGkRHBnqvfrxAry3j09d&radius=100&unit=km&locale=*&city=perth&countryCode=AU&stateCode=wa`;
         console.log(requestUrl);
         weather();
@@ -19,6 +17,10 @@ $(document).ready(function () {
             let events = data._embedded.events;
             console.log(data._embedded.events);
             for (i = 0, count = 1; i < events.length; i++, count++) {
+                if (count == 4) {
+                    count = 1
+                }
+                let  column = document.querySelector(`.column:nth-child(${count})`)
                 const element = events[i];
                 let eventName = document.createElement('h4');
                 let div = document.createElement('div');
@@ -31,10 +33,14 @@ $(document).ready(function () {
                 eventName.textContent = element.name;
                 div.appendChild(eventName);
                 div.appendChild(img);
-                if (count == 4) {
-                    count = 1
-                }
-                document.querySelector(`.column:nth-child(${count})`).append(div)
+
+                    column.appendChild(div)
+                    setTimeout(() => {
+                        $(div).fadeIn(500);
+                    }, i * 50);
+
+                //   $(`.event`).fadeIn(2000);
+            //querySelector(`.column:nth-child(${count})`).append(div)
             };
         });
     };
